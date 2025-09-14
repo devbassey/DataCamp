c89ca71efd7ea3f44c48b480a0574c7a97e36b3f
INYANG UTIBE BASSEY <bassey.utibe22@gmail.com>
2025-09-14 05:29:37 +0100

    Update practice.py

--- practice.py
+++ practice.py
@@ -30,3 +30,14 @@ print(myname)
 savings = 1000
 print(savings)
 
+
+# Create the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+
+
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+
+# Print new_savings
+print(new_savings)
