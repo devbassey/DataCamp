8979f25a83d3aecb62306fa73b6d406b6841cde3
INYANG UTIBE BASSEY <bassey.utibe22@gmail.com>
2025-09-14 05:24:50 +0100

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,32 @@
+# BMI CALCULATOR
+
+height = 60
+weight = 160
+bmi = weight / (height ** 2)
+print(bmi)
+
+if bmi < 18.5:
+    print("Underweight")
+elif bmi < 24.9:
+    print("Normal weight")
+elif bmi < 29.9:
+    print("Overweight")
+else:
+    print("Obesity")
+
+print("BMI Category")
+print("Underweight: <18.5")
+type(bmi)
+
+
+# Python strings
+firstname = "Utibe"
+lastname = "Abasi"
+myname = firstname + " " + lastname
+print(myname)
+
+
+# INTEGER
+savings = 1000
+print(savings)
+
